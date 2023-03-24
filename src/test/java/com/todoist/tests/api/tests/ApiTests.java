ab01332ffc5ef9f044ecc6dec88fde27901ff7ac
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2023-03-24 13:22:18 +0300

    change api test

--- src/test/java/com/todoist/tests/api/tests/ApiTests.java
+++ src/test/java/com/todoist/tests/api/tests/ApiTests.java
@@ -1,31 +1,17 @@
 package com.todoist.tests.api.tests;
 
 import com.todoist.tests.api.models.ListResponseAllProjectsModel;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.todoist.tests.api.models.ProjectsModel;
-import io.restassured.RestAssured;
-import io.restassured.common.mapper.TypeRef;
-import io.restassured.http.ContentType;
-//import io.restassured.mapper.ObjectMapper;
-import io.restassured.mapper.ObjectMapperType;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.io.DataInput;
-import java.util.List;
-
 import static com.todoist.tests.api.specs.LoginSpecs.loginRequestSpec;
 import static com.todoist.tests.api.specs.LoginSpecs.responseSpecification;
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatList;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ApiTests {
@@ -35,15 +21,16 @@ public class ApiTests {
     @Tag("api")
     @DisplayName("Получение всех проектов пользователя")
     public void receptionAllProjectTest() {
-        ListResponseAllProjectsModel authorization = given(loginRequestSpec)
+        Response authorization = given(loginRequestSpec)
                 .header("Authorization", "Bearer 9eb84cdd345a55ddf0ba278f893512e30670b4d1")
                 .when()
                 .get("/projects")
                 .then()
                 .spec(responseSpecification(200))
                 .extract()
-                .as(ListResponseAllProjectsModel.class);
+                .response();
 
-assertThat(authorization.getList().size()).isEqualTo(6);
+        int a = authorization.body().as(ListResponseAllProjectsModel.class).getList().size();
+        assertThat(a, is(6));
     }
 }
